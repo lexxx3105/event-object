a96bd31270a5d57480bc56b3cb41df3da09e4f59
lexxx3105 <129284703+lexxx3105@users.noreply.github.com>
2023-10-09 20:44:33 +0300

    dropdown/task.js

---
+++ dropdown/task.js
@@ -0,0 +1,25 @@
+const buttonsDropdown = document.querySelectorAll(`.dropdown__value`);
+
+function openDropdown() {
+
+  const dropdownList = this.closest(`.dropdown`).querySelector(`.dropdown__list`);
+  const buttonsLinks = dropdownList.querySelectorAll(`.dropdown__link`);
+
+  dropdownList.classList.toggle(`dropdown__list_active`);
+  buttonsLinks.forEach(button => {
+    button.onclick = choiceSetting;
+  })
+}
+
+function choiceSetting() {
+  this.closest(`.dropdown__list`).classList.remove(`dropdown__list_active`);
+  this.closest(`.dropdown`).querySelector(`.dropdown__value`).textContent = this.textContent;
+  
+  return false;
+}
+
+(function() {
+  buttonsDropdown.forEach(button => {
+    button.addEventListener(`click`, openDropdown);
+  });
+}());
